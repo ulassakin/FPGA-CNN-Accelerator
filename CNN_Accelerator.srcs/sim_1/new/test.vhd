028c5ca80a5875803ba6d97510a9e706487149fb
ulassakin <mulassakin@gmail.com>
2026-01-07 10:16:57 +0300

    testbench added

--- CNN_Accelerator.srcs/sim_1/new/test.vhd
+++ CNN_Accelerator.srcs/sim_1/new/test.vhd
@@ -1,43 +1,70 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 01/06/2026 12:38:33 PM
--- Design Name: 
--- Module Name: test - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity tb_MAC is
+end tb_MAC;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+architecture Behavioral of tb_MAC is
 
-entity test is
---  Port ( );
-end test;
+    signal clk   : std_logic := '0';
+    signal reset : std_logic := '0';
+    signal en    : std_logic := '0';
+    signal a     : signed(7 downto 0) := (others => '0');
+    signal b     : signed(7 downto 0) := (others => '0');
+    signal acc   : signed(15 downto 0);
 
-architecture Behavioral of test is
+    constant clk_period : time := 10 ns;
 
 begin
 
+   
+    DUT: entity work.MAC
+        port map (
+            clk     => clk,
+            reset   => reset,
+            en      => en,
+            a       => a,
+            b       => b,
+            acc_reg => acc
+        );
+
+   
+    clk_process : process
+    begin
+        clk <= '0';
+        wait for clk_period / 2;
+        clk <= '1';
+        wait for clk_period / 2;
+    end process;
+
+   
+    stim_proc : process
+    begin
+        reset <= '1';
+        wait for clk_period;
+        reset <= '0';
+
+        en <= '1';
+        a  <= to_signed(3, 8);
+        b  <= to_signed(4, 8);
+        wait for clk_period;
+
+        a <= to_signed(-2, 8);
+        b <= to_signed(5, 8);
+        wait for clk_period;
+
+        en <= '0';
+        a  <= to_signed(10, 8);
+        b  <= to_signed(10, 8);
+        wait for clk_period;
+
+        en <= '1';
+        a  <= to_signed(1, 8);
+        b  <= to_signed(6, 8);
+        wait for clk_period;
+
+        wait;
+    end process;
 
 end Behavioral;
